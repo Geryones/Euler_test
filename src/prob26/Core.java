f8bf0bb05baa705098fd61c4479da07c08f71410
jurij <the.jurij@gmail.com>
2015-02-11 12:14:47 +0100

    11.2.15 prob 26 gelöst

--- src/prob26/Core.java
+++ src/prob26/Core.java
@@ -1,10 +1,17 @@
 package prob26;
 
+import java.math.BigDecimal;
+
 /**
  * Created by mai714 on 09.02.2015.
  */
 public class Core {
     public static void main(String[] args) {
+
+
+      Reciprocal reci = new Reciprocal();
+        System.out.println( reci.findCycle(1000)[1]);
+
 
     }
 }
--- src/prob26/Reciprocal.java
+++ src/prob26/Reciprocal.java
@@ -1,7 +1,41 @@
 package prob26;
 
+import java.math.BigDecimal;
+
 /**
  * Created by mai714 on 09.02.2015.
  */
 public class Reciprocal {
+
+    public int[] findCycle(int max){
+        int sequenceLength=0;
+        int[] result = new int[2];
+
+        for (int i = max; i > 1; i--) {
+            if (sequenceLength >= i) {
+                break;
+            }
+
+            int[] foundRemainders = new int[i];
+            int value = 1;
+            int position = 0;
+
+            while (foundRemainders[value] == 0 && value != 0) {
+                foundRemainders[value] = position;
+                value *= 10;
+                value =value%i;
+                position++;
+            }
+
+            if (position - foundRemainders[value] > sequenceLength) {
+                sequenceLength = position - foundRemainders[value];
+               result[0]=sequenceLength;
+                result[1]=position;
+            }
+        }
+
+        return result ;
+
+    }
+
 }
